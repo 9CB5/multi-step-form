bac8edb638caee677a5c0e1cac3a40af6a5473de
Christian Barcelona <christianbarcelona95@gmail.com>
2023-12-17 15:32:37 +0000

    adjust head to fix accessibility errors

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -872,7 +872,7 @@ function createServerHead(options = {}) {
 
 const unheadPlugins = [];
 
-const appHead = {"meta":[{"name":"viewport","content":"width=device-width, initial-scale=1"},{"charset":"utf-8"}],"link":[],"style":[],"script":[],"noscript":[]};
+const appHead = {"meta":[{"name":"viewport","content":"width=device-width, initial-scale=1"},{"charset":"utf-8"}],"link":[],"style":[],"script":[],"noscript":[],"charset":"utf-8","viewport":"width=device-width, initial-scale=1","title":"multi-step-form","htmlAttrs":{"lang":"en"}};
 
 const appRootId = "__nuxt";
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'multi-step-form',
+      htmlAttrs: {
+        lang: 'en'
+      },
+    }
+  },
   ssr: false,
   devtools: { enabled: true },
   css: ["@/scss/main.scss"],
